9a5f6d947d25a4bc2bb07d04438637cbfc9773e0
Nikolaev Viktor <maclauddd@gmail.com>
2021-03-26 23:06:36 +0300

    added two routes - games and gameid

---
+++ pages/api/games/index.ts
@@ -0,0 +1,26 @@
+import axios from "axios";
+
+export default async (req, res) => {
+  const clientID = process.env.TWITCH_CLIENT_ID;
+  const secret = process.env.TWITCH_SECRET;
+  const igdbUrl = process.env.IGDB_BASE_URL;
+
+  // TODO check if auth not expired
+  const { data } = await axios.post(
+    `https://id.twitch.tv/oauth2/token?client_id=${clientID}&client_secret=${secret}&grant_type=client_credentials`
+  );
+
+  const response = await axios({
+    url: `${igdbUrl}/games`,
+    method: "POST",
+    headers: {
+      Accept: "application/json",
+      "Client-ID": clientID,
+      Authorization: `Bearer ${data.access_token}`,
+    },
+    data: `fields name, websites; limit 30;`,
+  });
+
+  res.statusCode = 200;
+  res.json({ games: response.data });
+};
