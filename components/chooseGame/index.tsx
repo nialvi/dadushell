d3a27c6500b72ebe9477174f6f12a7336dd3c6ae
Nikolaev Viktor <maclauddd@gmail.com>
2021-07-04 10:27:13 +0300

    draft bounced request

--- components/chooseGame/index.tsx
+++ components/chooseGame/index.tsx
@@ -1,33 +1,50 @@
 import axios from "axios";
-import { useState, useEffect } from "react";
+import React, { useState, useEffect } from "react";
+import { debounce } from "../../utils/debounce";
 
 interface IProps {
   onChoose: ({ name: string }) => void;
 }
 
+const getGameDebounced: ({ value, onSuccess }) => void = debounce(
+  ({ value, onSuccess }) => {
+    if (value) {
+      axios.get(`/api/games/${value}`).then(({ data }) => {
+        onSuccess(data.games);
+      });
+    }
+
+    console.log("request", value);
+  },
+  500
+);
+
 const ChooseGame = ({ onChoose }: IProps) => {
   const [isShowList, setShowList] = useState(false);
+  const [value, setValue] = useState("");
   const [list, setList] = useState([]);
 
-  const onChange = (e) => {
-    console.log("ek");
+  const onChange = (e: React.BaseSyntheticEvent) => {
+    setValue(e.target.value);
+
+    getGameDebounced({ value, onSuccess: setList });
   };
 
   const onClick = () => {
     setShowList(!isShowList);
   };
 
-  useEffect(() => {
-    if (!list.length) {
-      axios.get("/api/games").then(({ data }) => {
-        setList(data.games);
-      });
-    }
-  });
+  // useEffect(() => {
+  //   if (!list.length) {
+  //     axios.get("/api/games").then(({ data }) => {
+  //       setList(data.games);
+  //     });
+  //   }
+  // });
 
   return (
     <>
-      <input onClick={onClick} onChange={onChange} />
+      <input value={value} onClick={onClick} onChange={onChange} />
 
       {isShowList && (
         <ul>
